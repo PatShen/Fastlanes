81c1c80a4f63ea6fac3fc71a8f684c75e544b12e
PatShen <swxpat@qq.com>
2021-07-22 16:08:36 +0800

    将打包过程拆分成三个部分，提高多任务串行执行时对流水线的利用率，从而提升打包速度

--- fastlane/ios_fastfile.rb
+++ fastlane/ios_fastfile.rb
@@ -12,7 +12,6 @@ platform :ios do
 
   # desc '在最前执行...'
   # before_each do |options|
-  #   # build 号自动 +1
   #   increment_build_number
   #   git_add(path: "*.xcodeproj", shell_escape: false)
   #   reset_git_repo(
@@ -28,6 +27,127 @@ platform :ios do
   #     tags: false
   #   )
   # end
+
+
+  desc '打包前准备'
+  lane :tc_ios_package_prepare do |variable|
+    repoName = variable[:repo]
+    tc_pod_install(repo: repoName)
+  end
+
+  desc '打包'
+  lane :tc_ios_package_building do |variable|
+    scheme = variable[:scheme]
+    output = variable[:output_dir]
+    m = variable[:method]
+
+    puts "method: #{m}"
+
+    # app-store, package, ad-hoc, enterprise, development
+    @export_method = ""
+    # Release, Debug
+    @configuration = "Release"
+    # development, adhoc, enterprise, appstore
+    @match_type = ""
+
+    case m
+      when "debug"
+        @match_type = "development"
+        @export_method = "development"
+        @configuration = "Debug"
+      when "adhoc"
+        @match_type = "adhoc"
+        @export_method = "ad-hoc"
+      when "inhouse"
+        @match_type = "enterprise"
+        @export_method = "enterprise"
+      when "appstore"
+        @match_type = "appstore"
+        @export_method = "app-store"
+      else 
+        puts "不支持的打包类型"
+        return
+      end
+
+      match(
+        git_url: GIT_URL,
+        type: @match_type,
+        readonly: true
+      )
+
+      gym(
+        # 在构建前先clean
+        clean: true,
+        # 隐藏没有必要的信息
+        silent: true,
+        # 指定打包所使用的输出方式
+        export_method: @export_method,
+        # 指定项目的 scheme 名称
+        scheme: scheme,
+        # 指定输出的文件夹地址
+        output_directory: output,
+        # 指定打包方式
+        configuration: @configuration,
+      )
+  end
+
+  desc '上传'
+  lane :tc_ios_package_upload do |variable|
+    m = variable[:method]
+
+    if m == "appstore"
+      skipScreenshots = variable[:skip_screenshots]
+      skipMetadata = variable[:skip_metadata]
+      scheme = variable[:scheme]
+      
+      output = variable[:output_dir]
+      name = "#{SCHEME}.ipa"
+      info = tc_read_ipa_info(dir: output, name: name)
+      version = info[:CFBundleShortVersionString]
+
+      ipaPath = "#{output}/#{SCHEME}.ipa"
+
+      deliver(
+        ipa: ipaPath,
+        app_version: version,
+        automatic_release: false,
+        submit_for_review: true,
+        skip_screenshots: skipScreenshots,
+        overwrite_screenshots: !skipScreenshots,
+        skip_metadata: skipMetadata,
+        force: true,
+        team_name: "tospur co,.ltd"
+      )
+
+    else
+      env = variable[:env]
+      buildConfig = variable[:build_config]
+      scheme = variable[:scheme]
+      output = variable[:output_dir]
+      ipaPath = "#{output}/#{SCHEME}.ipa"
+
+      changelog = ""
+      changelogPath = File.expand_path('../changelog')
+      if File.exist?(changelogPath)
+        content = File.read(changelogPath)
+        if content.empty? == false
+          changelog = "--\n\n更新内容：\n\n#{content}"
+        end
+      end
+
+      result = tc_upload_pgy(
+        path: ipaPath,
+        env: env,
+        build_config: buildConfig,
+        change_log: changelog,
+        download_password: "tchf"
+      )
+      puts result
+      tc_post_dingtalk(msg_type: "markdown", title: "打包通知", text: result)
+    end
+
+  end
+
 
   desc '打Debug包'
   lane :tc_ios_package_debug do |options|
